8783a85f94cae390f0d3f05cf0d40078b49bbfc1
Harjyot Kaur <harjyotkaur.1409@gmail.com>
2019-02-26 19:39:06 -0800

    updated text_quality test to ensure branch coverage

--- tests/testthat/test_text_quality.R
+++ tests/testthat/test_text_quality.R
@@ -13,30 +13,50 @@ context("Test text_quality")
 
 
 
-# Sample input
-text <-  "This str has words spelllll wrong. This string has a slag word shitty."
-
-# sample output
-df <- data.frame(spell_error=0.15, toxicity=0.08)
-
-
-
 #' Test for output type
 
 test_that("Test that output is of type dataframe", {
 
-  expect_true(class(text_quality(text))=="data.frame")
+  txt <-  "This str has words spelllll wrong. This string has a slag word shitty."
+
+  expect_true(class(text_quality(txt))=="data.frame")
 
 })
 
 
 #' Test for values returned in the dataframe
-test_that("Test that output contains double", {
+test_that("Test that spell error and toxic_words are lists", {
 
-  output <- text_quality(text)
+  txt <-  "This str has words spelllll wrong. This string has a slag word shitty."
 
-  expect_true(typeof(output$spell_error)=="double")
-  expect_true(typeof(output$toxicity)=="double")
+  output <- text_quality(txt)
+
+  expect_true(typeof(output$spell_error)=="list")
+  expect_true(typeof(output$toxic_words)=="list")
+
+})
+
+#' Test for values returned in the dataframe
+test_that("Test that count of spell error and toxic_words is integer", {
+
+  txt <-  "This str has words spelllll wrong. This string has a slag word shitty."
+
+  output <- text_quality(txt)
+
+  expect_true(typeof(output$count_spell_error)=="integer")
+  expect_true(typeof(output$count_toxic_words)=="integer")
+
+})
+
+#' Test for values returned in the dataframe
+test_that("Test that proportion of spell error and toxic_words is double", {
+
+  txt <-  "This str has words spelllll wrong. This string has a slag word shitty."
+
+  output <- text_quality(txt)
+
+  expect_true(typeof(output$proportion_spell_error)=="double")
+  expect_true(typeof(output$proportion_toxic_words)=="double")
 
 })
 
@@ -44,64 +64,105 @@ test_that("Test that output contains double", {
 #' Test for values returned in the dataframe
 test_that("Test that output contains non-negative values", {
 
-  output <- text_quality(text)
+  txt <-  "This str has words spelllll wrong. This string has a slag word shitty."
 
-  expect_true(typeof(output$spell_error)>=0)
-  expect_true(typeof(output$toxicity)>=0)
+  output <- text_quality(txt)
 
-})
-
-
-#' Test for valid input
-test_that("Input should be a string", {
-
-  text <- 123
-
-  expect_error(text_quality(text))
+  expect_true(typeof(output$count_spell_error)>=0)
+  expect_true(typeof(output$count_spell_error)>=0)
+  expect_true(typeof(output$proportion_spell_error)>=0)
+  expect_true(typeof(output$proportion_toxic_words)>=0)
 
 })
 
 
-#' Test for valid input
-test_that("Input should be a string", {
 
-  text <- ""
+#' Test for valid output
+test_that("Output should be empty", {
 
-  expect_error(text_quality(text))
+
+  output <- text_quality("")
+
+  expect_true(output$spell_error=="")
+  expect_true(output$toxic_words=="")
+  expect_true(output$count_spell_error==0)
+  expect_true(output$count_spell_error==0)
+  expect_true(output$proportion_spell_error==0)
+  expect_true(output$proportion_toxic_words==0)
+
 
 })
 
 
-#' Test for valid input
-test_that("Input should be a string", {
+#' Test for valid output
+test_that("Output should be empty", {
 
-  text <- "!@#^&*(*&!&@_@_@)((@}}}}"
+  txt <- "!@#^&*(*&!&@_@_@)((@}}}}"
 
-  expect_error(text_quality(text))
+  output <- text_quality(txt)
+
+  expect_true(output$spell_error=="")
+  expect_true(output$toxic_words=="")
+  expect_true(output$count_spell_error==0)
+  expect_true(output$count_spell_error==0)
+  expect_true(output$proportion_spell_error==0)
+  expect_true(output$proportion_toxic_words==0)
 
 })
 
 
 
 #' Test for functionality
-test_that("Test that spell error gives expected output", {
+test_that("Test that text_quality gives the expected output", {
 
-  output <- text_quality(text)
+  txt <-  "This string is correct."
 
-  expect_true(output$spell_error>=df$spell_error-0.02 & output$spell_error<=df$spell_error+0.02)
+  output <- text_quality(txt)
+
+  expect_true(output$spell_error=="")
+  expect_true(output$toxic_words=="")
+  expect_true(output$count_spell_error==0)
+  expect_true(output$count_spell_error==0)
+  expect_true(output$proportion_spell_error==0)
+  expect_true(output$proportion_toxic_words==0)
 
 })
 
 
 #' Test for functionality
-test_that("Test that toxicity gives expected output", {
+test_that("Test that text_quality gives the expected output", {
 
-  output <- text_quality(text)
+  txt <-  "This string is correct and has pronouns Harjyot, Alex, Yenan."
 
-  expect_true(output$toxicity>=df$toxicity-0.02 & output$toxicity<=df$toxicity+0.02)
+  output <- text_quality(txt)
+
+  expect_true(output$spell_error=="")
+  expect_true(output$toxic_words=="")
+  expect_true(output$count_spell_error==0)
+  expect_true(output$count_spell_error==0)
+  expect_true(output$proportion_spell_error==0)
+  expect_true(output$proportion_toxic_words==0)
 
 })
 
+
+#' Test for functionality
+test_that("Test that text_quality gives the expected output", {
+
+  txt <-  "This string has words spelllll wrong. This string has a slag word shitty."
+
+  output <- text_quality(txt)
+
+  expect_true(output$spell_error[[1]][1]=="spelllll")
+  expect_true(output$toxic_words[[1]][1]=="shitty")
+  expect_true(output$count_spell_error==1)
+  expect_true(output$count_toxic_words==1)
+  expect_true(output$proportion_spell_error>=0.07692308-0.02 &
+                output$proportion_spell_error<=0.07692308+0.02)
+  expect_true(output$proportion_toxic_words>=0.07692308-0.02 &
+                output$proportion_toxic_words<=0.07692308+0.02)
+
+})
 
 
 
